37980393d9bc7abd549b80e1614928968a26765e
Mirabelle SALLES <mirabelle.salles@univ-rouen.fr>
2026-02-26 16:37:57 +0100

    fix: re-updated file structure to follow the QA's directives

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Test } from './components/widgets/test/test';
+import { Test } from './shared/components/test/test';
 
 export const routes: Routes = [
     {path: '', component: Test},
