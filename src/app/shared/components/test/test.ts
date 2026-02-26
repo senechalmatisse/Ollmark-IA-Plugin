b1ad5da43b690029fccca8aad65b7d6322b7f356
Mirabelle SALLES <mirabelle.salles@univ-rouen.fr>
2026-02-26 17:02:01 +0100

    feat: added SearchBar component

--- src/app/shared/components/test/test.ts
+++ src/app/shared/components/test/test.ts
@@ -1,24 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { TestService } from '../../utils/test-service/test-service';
-import { OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {SearchBar} from '../../../features/OLM-1102/ui/search-bar/search-bar';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchBar
+  ],
   templateUrl: './test.html',
   styleUrl: './test.css',
 })
-export class Test implements OnInit {
-  private testService = inject(TestService);
-
-
-  message = "";
-
-  ngOnInit(): void {
-    this.testService.getMessageTest().subscribe((message) => {
-      this.message = message;
-    });
-  }
-
+export class Test {
 }
