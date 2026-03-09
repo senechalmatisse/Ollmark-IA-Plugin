2f0825f8e6acd2ce77e0fe53eab646a3badacb18
Tarek MOUSSAOUI <tarek.moussaoui22007@gmail.com>
2026-03-09 15:09:04 +0100

    fixing the bug of the front test !

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'] 
 })
 export class App {
-  protected readonly title = signal('Ollmark-plugin-ia');
+  public readonly title = 'Ollmark-plugin-ia'; 
 }
