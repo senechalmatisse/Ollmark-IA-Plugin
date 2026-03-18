c3a35e467b9906e87b39d8408d0702decebd3305
amineBelkessa <amine.belkessa@univ-rouen.fr>
2026-03-18 16:40:23 +0100

    fix : delelte randomuuid

--- src/app/core/services/chat-state.service.ts
+++ src/app/core/services/chat-state.service.ts
@@ -105,12 +105,9 @@ export class ChatStateService {
         return id;
     }
     private generateId(): string {
-        if (typeof crypto !== 'undefined' && crypto.randomUUID) {
-            return crypto.randomUUID();
-        }
+     // ❌ on ignore complètement crypto (trop instable dans ton env)
 
-        // fallback propre
-        return 'xxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
             const r = Math.random() * 16 | 0;
             const v = c === 'x' ? r : (r & 0x3 | 0x8);
             return v.toString(16);
